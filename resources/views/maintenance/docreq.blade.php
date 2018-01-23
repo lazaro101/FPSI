927b983707d693d184d2b1f84df6f476e569b302
Aubrey Vidanes <vidanesaubrey@yahoo.com>
2018-01-24 00:28:40 +0800

    label

--- resources/views/maintenance/docreq.blade.php
+++ resources/views/maintenance/docreq.blade.php
@@ -108,7 +108,7 @@
               <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title">Edit General Requirements</h4>
+                <h4 class="modal-title">Edit Documentary Requirements</h4>
               </div>
               <div class="modal-body">
                 <div class="form-group">
